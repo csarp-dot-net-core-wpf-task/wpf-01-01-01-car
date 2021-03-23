013096c4138d4d3d48ef760058d1a1b32c0a3c45
gycsaba <gyuris.csaba@vasvari.org>
2021-03-23 10:05:46 +0100

    wpf-01-01-01-car projekt

---
+++ wpf-01-01-01-car/model/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wpf_01_01_01_car.model
+{
+    /// <summary>
+    /// Autó osztály
+    /// </summary>
+    class Car
+    {
+       
+    }
+}
