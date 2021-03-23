fbf43a772bcd5d9617ac600300c2e097cd9c0bab
gycsaba <gyuris.csaba@vasvari.org>
2021-03-23 10:05:56 +0100

    Autó osztály kódja

--- wpf-01-01-01-car/model/Car.cs
+++ wpf-01-01-01-car/model/Car.cs
@@ -9,6 +9,33 @@ namespace wpf_01_01_01_car.model
     /// </summary>
     class Car
     {
-       
+        /// <summary>
+        /// tulajdonos
+        /// </summary>
+        private string owner;
+
+        /// <summary>
+        /// Konstruktor (autó tulajdonos nevének megadásával jön létre)
+        /// </summary>
+        /// <param name="owner">autó tulajdonos</param>
+        public Car(string owner)
+        {
+            this.owner = owner;
+        }
+
+        /// <summary>
+        /// az autó elmegy a munkahelyre
+        /// </summary>
+        public string GoToWorkString()
+        {
+            return owner + " autója tulajdonosát elviszi a munkahelyre.";
+        }
+        /// <summary>
+        /// Az autó hazajön a munkahelyről.
+        /// </summary>
+        public string ComingHomeString()
+        {
+            return owner + " autója tulajdonosát elviszi haza.";
+        }
     }
 }
